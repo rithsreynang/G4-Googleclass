f998bb2e5f5ebf191381b1e757871a43db952c20
RADY.DOEURN <RADY.DOEURN@PNC.LAN>
2024-02-16 10:32:40 +0700

    [feat]create sign up interfacer #G4-53

--- views/authentication/signup.view.php
+++ views/authentication/signup.view.php
@@ -30,76 +30,91 @@
 <body>
 	<!-- **************** MAIN CONTENT START **************** -->
 	<main>
-		<section class="p-0 d-flex align-items-center position-relative overflow-hidden">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-12  col-lg-9 m-auto">
-						<div class="row  my-5">
-							<div class="col-sm-10 col-xl-8 m-auto border rounded">
-								<!-- Title -->
-								<span class="mb-0 fs-1">👋</span>
-								<h1 class="fs-2 text-center">Create Account !</h1>
-								<!-- <p class="lead mb-4">Nice to see you! Please log in with your account.</p> -->
-								<!-- Form START -->
-								<form action="../../controllers/authentication/create_user.controller.php" method="post">
-									<!-- Username -->
-									<div class="mb-4">
-										<label for="exampleInputEmail1" class="form-label">Username *</label>
-										<div class="input-group input-group-lg">
-											<span class="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i class="bi bi-person-fill"></i></span>
-											<input type="text" name="username" class="form-control border-0 bg-light rounded-end ps-1" placeholder="Username" id="exampleInputUsername">
-										</div>
-									</div>
-									<!-- Email -->
-									<div class="mb-4">
-										<label for="exampleInputEmail1" class="form-label">Email address *</label>
-										<div class="input-group input-group-lg">
-											<span class="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i class="bi bi-envelope-fill"></i></span>
-											<input type="email" name="email" class="form-control border-0 bg-light rounded-end ps-1" placeholder="E-mail" id="exampleInputEmail1">
-										</div>
-									</div>
-									<!-- Password -->
-									<div class="mb-4">
-										<label for="inputPassword5" class="form-label">Password *</label>
-										<div class="input-group input-group-lg">
-											<span class="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i class="fas fa-lock"></i></span>
-											<input type="password" name="password" class="form-control border-0 bg-light rounded-end ps-1" placeholder="password" id="inputPassword5">
-										</div>
-										<div id="passwordHelpBlock" class="form-text">
-											Your password must be 8 characters at least
-										</div>
-									</div>
-									<!-- Check box -->
+  <section class="p-0 d-flex align-items-center position-relative overflow-hidden">
+  
+    <div class="container-fluid">
+      <div class="row">
+        <!-- left -->
+        <div class="col-12 col-lg-6 d-md-flex align-items-center justify-content-center bg-success bg-opacity-10 vh-lg-100">
+          <div class="p-3 p-lg-5">
+            <!-- Title -->
+            <div class="text-center">
+              <h2 class="fw-bold">Welcome to PNC-classroom</h2>
+              
+            </div>
+            <!-- SVG Image -->
+            <img src="assets/images/element/06.svg" class="mt-7" alt="">
+            
+          </div>
+        </div>
 
-									<!-- Button -->
-									<div class="align-items-center mt-0">
-										<div class="d-grid">
-											<button class="btn btn-primary mb-0" type="submit">Sign up</button>
-										</div>
-									</div>
-								</form>
-								<!-- Form END -->
-								<!-- Sign up link -->
-								<div class="mt-4 text-center">
-									<span>have account already? <a href="user-signin">Sign in here</a></span>
-								</div>
-							</div>
-						</div> <!-- Row END -->
-					</div>
-				</div> <!-- Row END -->
-			</div>
-		</section>
-	</main>
-	<!-- **************** MAIN CONTENT END **************** -->
-	<!-- Back to top -->
-	<div class="back-top"><i class="bi bi-arrow-up-short position-absolute top-50 start-50 translate-middle"></i></div>
+        <!-- Right -->
+        <div class="col-12 col-lg-6 m-auto">
+          <div class="row my-5">
+            <div class="col-sm-10 col-xl-8 m-auto">
+              <!-- Title -->
+              <h1 class="fs-2">Create Account !</h1>
+              <!-- <p class="lead mb-4">Nice to see you! Please log in with your account.</p> -->
+			  <!-- Form START -->
+              <form>
+				<!-- Usernam -->
+				<div class="mb-4">
+                  <label for="InputUsername"  class="form-label">Username *</label>
+                  <div class="input-group input-group-lg">
+                    <span class="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i class="fa fa-user"></i></span>
+                    <input type="username" class="form-control border-0 bg-light rounded-end ps-1" placeholder="username" id="exampleInputUsername">
+                  </div>
+                </div>
+                <!-- Email -->
+                <div class="mb-4">
+                  <label for="exampleInputEmail1" class="form-label">Email address *</label>
+                  <div class="input-group input-group-lg">
+                    <span class="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i class="bi bi-envelope-fill"></i></span>
+                    <input type="email" class="form-control border-0 bg-light rounded-end ps-1" placeholder="E-mail" id="exampleInputEmail1">
+                  </div>
+                </div>
+                <!-- Password -->
+                <div class="mb-4">
+                  <label for="inputPassword5" class="form-label">Password *</label>
+                  <div class="input-group input-group-lg">
+                    <span class="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i class="fas fa-key"></i></span>
+                    <input type="password" class="form-control border-0 bg-light rounded-end ps-1" placeholder="password" id="inputPassword5">
+                  </div>
+                  <div id="passwordHelpBlock" class="form-text">
+                    Your password must be 8 characters at least 
+                  </div>
+                </div>
+                <!-- Button -->
+                <div class="align-items-center mt-0">
+                  <div class="d-grid">
+                    <button class="btn btn-lg btn-success mb-0" type="button">Sign Up</button>
+                  </div>
+                </div>
+              </form>
+              <!-- Form END -->
 
-	<!-- Bootstrap JS -->
-	<script src="vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+              
+              <!-- Sign up link -->
+              <div class="mt-4 text-center">
+                <span> Have an account? <a href="sign-in.html">Login here</a></span>
+              </div>
+            </div>
+          </div> <!-- Row END -->
+        </div>
+      </div> <!-- Row END -->
+    </div>
+  </section>
+</main>
+<!-- **************** MAIN CONTENT END **************** -->
 
-	<!-- Template Functions -->
-	<script src="vendor/js/functions.js"></script>
+<!-- Back to top -->
+<div class="back-top"><i class="bi bi-arrow-up-short position-absolute top-50 start-50 translate-middle"></i></div>
+
+<!-- Bootstrap JS -->
+<script src="vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- Template Functions -->
+<script src="vendor/js/functions.js"></script>
 
 </body>
-
 </html>
