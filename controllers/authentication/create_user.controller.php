a9d690f32653ee8ffe447457de82b50893667340
SENGHAK.CHHUN <SENGHAK.CHHUN@pnc.lan>
2024-02-17 08:47:48 +0700

    [feat] update sign up #G4-6

--- controllers/authentication/create_user.controller.php
+++ controllers/authentication/create_user.controller.php
@@ -1,28 +1,25 @@
 <?php
-    session_start();
-    require "../../models/signup.model.php";
-    require "../../database/database.php";
-    $statement = $connection->prepare("select * from users");
-    $statement->execute();
-    $emails = $statement->fetchAll();
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){        
-        if (!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])){
-            $username = htmlspecialchars($_POST['username']);
-            $email = htmlspecialchars($_POST['email']);
-            $password = htmlspecialchars($_POST['password']);
+session_start();
+require "../../models/signup.model.php";
+require "../../database/database.php";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+        $username = htmlspecialchars($_POST['username']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+        $user = accountExist($email);
+        if (strlen($password) > 8) {
             $password_encrypt = password_hash($password, PASSWORD_BCRYPT);
-            $user = accountExist($email);
-            if (count($user) == 0){
+            if (count($user) == 0) {
                 createAccount($username, $email, $password_encrypt);
                 header("Location: /home");
-                $_SESSION['success'] = "Account Created successfully";
-            }else{
-                echo "Account already exists";
+            } else {
+                header("Location: /user-signup");
             }
-        }
-        else{
-            echo "<script>alert('Missing!!')</script>";
+        } else {
             header("Location:  /user-signup");
         }
+    } else {
+        header("Location:  /user-signup");
     }
-?>
+}
--- views/authentication/signup.view.php
+++ views/authentication/signup.view.php
@@ -26,6 +26,10 @@
   <link id="style-switch" rel="stylesheet" type="text/css" href="vendor/css/style.css">
 
 </head>
+<?php
+echo '<script>alert("Please complete Your name, email and password to create account with E-Classroom Correctly!!")</script>';
+
+?>
 
 <body>
   <!-- **************** MAIN CONTENT START **************** -->
@@ -59,6 +63,9 @@
                     <div class="input-group input-group-lg">
                       <span class="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i class="fa fa-user"></i></span>
                       <input type="username" name="username" class="form-control border-0 bg-light rounded-end ps-1" placeholder="username" id="exampleInputUsername">
+                      <div id="passwordHelpBlock" class="form-text">
+                      Must have username
+                    </div>
                     </div>
                   </div>
                   <!-- Email -->
@@ -68,6 +75,9 @@
                       <span class="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i class="bi bi-envelope-fill"></i></span>
                       <input type="email" name='email' class="form-control border-0 bg-light rounded-end ps-1" placeholder="E-mail" id="exampleInputEmail1">
                     </div>
+                    <div id="passwordHelpBlock" class="form-text">
+                      Can not create with email that have create before!
+                    </div>
                   </div>
                   <!-- Password -->
                   <div class="mb-4">
@@ -75,6 +85,7 @@
                     <div class="input-group input-group-lg">
                       <span class="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i class="fas fa-key"></i></span>
                       <input type="password" name="password" class="form-control border-0 bg-light rounded-end ps-1" placeholder="password" id="inputPassword5">
+
                     </div>
                     <div id="passwordHelpBlock" class="form-text">
                       Your password must be 8 characters at least
