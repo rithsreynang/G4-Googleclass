d7021ea230941cb11c56edc614bb457ad8286e4a
sreymom.quiz <SREYMOM.CHAK@pnc.lan>
2024-02-18 14:45:29 +0700

    update code home

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,32 +1,33 @@
 <!-- Header START -->
 <header class="navbar-light navbar-sticky header-static">
-	<!-- Logo Nav START -->
-	<nav class="navbar navbar-expand-xl">
-		<div class="container-fluid px-3 px-xl-5">
-			<!-- Logo START -->
-			<a class="navbar-brand" href="/">
-				<h3>
-					<span class="text-danger">E-</span>
-					<span>Classroom</span>
-				</h3>
-				<!-- <img class="light-mode-item navbar-brand-item" src="assets/images/logo.svg" alt="logo"> -->
-				<!-- <img class="dark-mode-item navbar-brand-item" src="assets/images/logo-light.svg" alt="logo"> -->
-			</a>
-			<!-- Logo END -->
-			<a href="#">
-				<h3>For Education</h3>
-			</a>
-			<div class="d-flex">
-				<a href="/user-signup">
-					<h3 class="btn bg-danger text-light">sign up</h3>
-				</a>
-				<a href="/user-signin">
-					<h3 class="btn bg-light">sign in</h3>
-				</a>
-			</div>
+    <!-- Logo Nav START -->
+    <nav class="navbar navbar-expand-xl">
+        <div class="container-fluid px-3 px-xl-5">
+            <!-- Logo START -->
+            <a class="navbar-brand" href="/">
+                <h3>
+                    <span class="text-danger display-6">E</span>-Classroom
+                </h3>
+                <!-- <img class="light-mode-item navbar-brand-item" src="assets/images/logo.svg" alt="logo"> -->
+                <!-- <img class="dark-mode-item navbar-brand-item" src="assets/images/logo-light.svg" alt="logo"> -->
+            </a>
+            <!-- Logo END -->
+            <a href="#">
+                <h3>
+                    <span class="text-danger display-6">E</span>ducation
+                </h3>
+            </a>
+            <div class="d-flex ml-1">
+                <a href="/user-signup">
+                    <h3 class="btn bg-danger text-light">sign up</h3>
+                </a>
+                <a href="/user-signin">
+                    <h3 class="btn bg-light">sign in</h3>
+                </a>
+            </div>
 
-		</div>
-	</nav>
-	<!-- Logo Nav END -->
+        </div>
+    </nav>
+    <!-- Logo Nav END -->
 </header>
 <!-- Header END -->
