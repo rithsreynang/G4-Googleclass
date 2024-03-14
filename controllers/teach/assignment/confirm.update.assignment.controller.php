4b404b9985ac09eb384fd4973bcc4ee9e38017bc
SENGHAK.CHHUN <SENGHAK.CHHUN@pnc.lan>
2024-03-14 20:20:29 +0700

    [feat] delete material or lesson #G4-43

--- controllers/teach/assignment/confirm.update.assignment.controller.php
+++ controllers/teach/assignment/confirm.update.assignment.controller.php
@@ -7,31 +7,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $description = $_POST['description'];
     $score = $_POST['fullscore'];
     $dateline = $_POST['dateline'];
-    $file = $_FILES['file'];
-    $fileName = $file['name'];
-    $fileTemName = $_FILES['file']['tmp_name'];
-    $fileSize = $file['size'];
-    $fileError = $file['error'];
-    $fileType = $file['type'];
-    $fileExt = explode('.', $fileName);
-    $fileActualExt = strtolower(end($fileExt));
-    $allowed = array('jpg', 'png', 'jpeg', 'pdf', 'pptx', 'zip', 'txt', 'docx', 'gif', 'xlsx', 'html', 'json', 'js', 'css');
-    $postDate = date("Y-m-d h:i:sa");
-    if ($file) {
-        if (in_array($fileActualExt, $allowed)) {
-            if ($fileError == 0) {
-                if ($fileSize < 100000000) {
-                    $fileNameNew = uniqid('', 'true') . "." . $fileActualExt;
-                    $fileDestination = '../../../assets/files/' . $fileNameNew;
-                    move_uploaded_file($fileTemName, $fileDestination);
-                    print_r($fileName);
-                    print_r($fileDestination);
+    if (isset($_FILES['file'])){
+        $file = $_FILES['file'];
+        $fileName = $file['name'];
+        $fileTemName = $_FILES['file']['tmp_name'];
+        $fileSize = $file['size'];
+        $fileError = $file['error'];
+        $fileType = $file['type'];
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+        $allowed = array('jpg', 'png', 'jpeg', 'pdf', 'pptx', 'zip', 'txt', 'docx', 'gif', 'xlsx', 'html', 'json', 'js', 'css');
+        $postDate = date("Y-m-d h:i:sa");
+        if ($file) {
+            if (in_array($fileActualExt, $allowed)) {
+                if ($fileError == 0) {
+                    if ($fileSize < 100000000) {
+                        $fileNameNew = uniqid('', 'true') . "." . $fileActualExt;
+                        $fileDestination = '../../../assets/files/' . $fileNameNew;
+                        move_uploaded_file($fileTemName, $fileDestination);
+                        print_r($fileName);
+                        print_r($fileDestination);
+                    }
                 }
+            } else {
+                echo "<script> alert('can not upload this file') </script>";
             }
-        } else {
-            echo "<script> alert('can not upload this file') </script>";
         }
+        $assignment = updateAssignment($assignment_id, $title, $description, $fileDestination, $score, $dateline, $fileName);
     }
-    $assignment = updateAssignment($assignment_id, $title, $description, $fileDestination, $score, $dateline, $fileName);
     header("Location: ../classwork.controller.php?classroom_id= $classroom_id");
 }
--- controllers/teach/assignment/delete.assignment.controller.php
+++ controllers/teach/assignment/delete.assignment.controller.php
@@ -1,7 +1,11 @@
 <?php
-    require_once "../../layouts/class/header.php";
-    $assignment_id = $_GET['assignment_id'];
-    echo $assignment_id;
-    require_once "../../views/teach/delete.assignment.view.php";
-?>
+require_once "../../../models/teach/assignment/delete.assignment.model.php";
 
+$classroom_id = $_GET['classroom_id'];
+$assignment_id = $_GET['assignment_id'];
+echo $classroom_id;
+echo $assignment_id;
+deleteAssignment($assignment_id);
+
+header("Location: ../classwork.controller.php?classroom_id=$classroom_id");
+exit();
