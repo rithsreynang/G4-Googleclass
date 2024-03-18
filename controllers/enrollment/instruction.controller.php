8e6cda984e8e1f2fae876584b74117d792bdc351
THEUN.EN <THEUN.EN@PNC.LAN>
2024-03-18 11:21:28 +0700

    [feat] view student work on assignment #G4-87

--- controllers/enrollment/instruction.controller.php
+++
@@ -1,3 +0,0 @@
-<?php
-    require_once "../../views/enrollment/instuction.view.php";
-?>
--- user.router.php
+++ user.router.php
@@ -19,6 +19,7 @@ $routes = [
     '/grade-teacher' => 'controllers/teach/grade/grades.controller.php',
     '/change-banner-class' => 'controllers/classroom/change.banner.controller.php',
     '/instruction-assignment' => 'controllers/teach/assignment/view.assignment/go.instruction.contoller.php',
+    '/view-instruction-assignment' => 'controllers/enrollment/view-assignment/go.instruction.assignment.controller.php',
     '/student-work' => 'controllers/teach/assignment/view.assignment/go.student.work.controller.php'
 ];
 if (array_key_exists($uri, $routes)) {
