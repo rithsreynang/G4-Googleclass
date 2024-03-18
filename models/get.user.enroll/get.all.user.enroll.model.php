ff5e080d21e5792d1ca8f80ed33af0099eb00ec7
THEUN.EN <THEUN.EN@pnc.lan>
2024-03-18 10:15:07 +0700

    [feat] show detail students that have submit and don’t submit assignment. #G-87

--- models/get.user.enroll/get.all.user.enroll.model.php
+++ models/get.user.enroll/get.all.user.enroll.model.php
@@ -1,14 +1,29 @@
 <?php
-    require_once "database/database.php";
-    function getAllUserEnroller($classroom_id):array
-    {
-        global $connection;
-        $statement = $connection->prepare("SELECT * FROM assignments INNER JOIN classroom_enroll on assignments.classroom_id = classroom_enroll.classroom_id WHERE assignments.classroom_id = :classroom_id and classroom_enroll.role ='student';");
-        $statement->execute([":classroom_id" => $classroom_id]);
-        if ($statement->rowCount() > 0){
-            return $statement->fetch();
-        }else{
-            return [];
-        }
+require_once "database/database.php";
+function getAllUserEnroller($classroom_id): array
+{
+    global $connection;
+    $statement = $connection->prepare("SELECT * FROM assignments INNER JOIN classroom_enroll on assignments.classroom_id = classroom_enroll.classroom_id WHERE assignments.classroom_id = :classroom_id and classroom_enroll.role ='student';");
+    $statement->execute([":classroom_id" => $classroom_id]);
+    if ($statement->rowCount() > 0) {
+        return $statement->fetch();
+    } else {
+        return [];
     }
-?>
+}
+
+function getAllstudentEnroller($classroom_id): array
+{
+    global $connection;
+    $statement = $connection->prepare("SELECT classroom_enroll.class_enroll_id,
+    users.user_id, users.username, users.email, users.profile from classroom_enroll
+    inner JOIN users ON users.user_id = classroom_enroll.user_id where classroom_id =:id and role='student'");
+    $statement->execute([
+        ":id" => $classroom_id
+    ]);
+
+    if ($statement->rowCount() > 0) {
+        return $statement->fetchAll();
+    }
+    return [];
+}
