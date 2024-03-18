f26ba697e3c0b5493ac6544a01f3bef42fa115f2
SENGHAK.CHHUN <SENGHAK.CHHUN@PNC.LAN>
2024-03-18 10:35:13 +0700

    [feat] show link assignment on upcoming that have assign #G4-88

--- views/home/home.view.php
+++ views/home/home.view.php
@@ -222,11 +222,11 @@
                     <ul style="list-style-type: none" class="d-flex ">
                         <li class="mr-1">
                             <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#JoinModal">
                                 Join class
                             </button>
                             <!-- Modal -->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal fade" id="JoinModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
                                     <form action="#" method="post">
                                         <div class="modal-content">
@@ -258,7 +258,7 @@
                                     <div class="modal-dialog" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                <h5 class="modal-title" id="exampleModalLabel">Create Class</h5>
                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                     <span aria-hidden="true">&times;</span>
                                                 </button>
