c8e5ad32bc3bb29230e47ef16519d5baa2582e6f
THEUN.EN <THEUN.EN@pnc.lan>
2024-03-20 07:32:05 +0700

    [feat] merge code from develop #G4-87

--- views/enrollment/enrollment.view.php
+++ views/enrollment/enrollment.view.php
@@ -61,7 +61,7 @@ $index = 0;
                                                 <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1z" />
                                             </svg>
                                         </div>
-                                        <p class="ml-2 mt-3"><?= $assignment['title'] ?></p>
+                                        <a class="ml-2" href="controllers/enrollment/view-assignment/instruction.controller.php?classroom_id=<?= $assignment['classroom_id'] ?>&assignment_id=<?= $assignment['assignment_id'] ?>" style="color: black;"><?= $assignment['title'] ?></a>
                                     </div>
                                     <div class="d-flex align-items-center justify-content-between">
                                         <p class="mt-3 fw-bold" style="margin-right: -20px;">
@@ -84,10 +84,10 @@ $index = 0;
                                 </div>
                                 <div style="margin-right: 20px;">
                                     <div class="dropdown" style="color: blue">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="dropdown-toggle" type="button" id="dropdownMenuassignment" data-bs-toggle="dropdown" aria-expanded="false" width="22" height="22" fill="currentColor" class="bi bi-three-dots-vertical d-flex justify-content-center mr-5" viewBox="0 0 16 16">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="dropdown-toggle" type="button" id="viewAssignment" data-bs-toggle="dropdown" aria-expanded="false" width="22" height="22" fill="currentColor" class="bi bi-three-dots-vertical d-flex justify-content-center mr-5" viewBox="0 0 16 16">
                                             <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
                                         </svg>
-                                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuassignment">
+                                        <ul class="dropdown-menu" aria-labelledby="viewAssignment">
                                             <li><a class="dropdown-item" href="#">Copy Link</a></li>
                                         </ul>
                                     </div>
